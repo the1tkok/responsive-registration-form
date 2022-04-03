5a22ea89f72e471ccefd03cf1bec98e14217c6ea
the1tkok <the1tkok@gmail.com>
2022-04-03 12:38:14 -0500

    .

---
+++ app.js
@@ -0,0 +1,24 @@
+const backgroundImg = document.querySelector('.side-background-img')
+const img = document.createElement('img');
+img.className = 'plant-img';
+img.src = 'plant-img.jpg';
+
+const backgroundOverlay = document.querySelector('.background-overlay');
+const imgOverlay = document.createElement('img');
+imgOverlay.className = 'logo-img';
+imgOverlay.src = 'odin-logo.png';
+
+const logoText = document.querySelector('.logo-text');
+
+const mediaQ = window.matchMedia("(max-width: 926px")
+
+function removeImg() {
+        if (!mediaQ.matches) {
+            backgroundOverlay.insertBefore(imgOverlay, logoText);
+            backgroundImg.appendChild(img);
+        } else {
+            backgroundImg.innerHTML = "";
+        }
+    }
+
+window.addEventListener('load', removeImg)
